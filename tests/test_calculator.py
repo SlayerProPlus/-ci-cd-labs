4044fe545bd2930cf35538a2c841ecb4ddfabaa0
SlayerProPlus <josedparedesf@gmail.com>
2026-08-16 19:12:29 -0400

    fix: corregir prueba de sumar

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,7 +6,7 @@ from app.calculator import sumar, restar, multiplicar, dividir
 import pytest
 
 def test_sumar():
-    assert sumar(3, 2) == 99
+    assert sumar(3, 2) == 5
 
 def test_restar():
     assert restar(10, 4) == 6
